a38b578a278d84dad82800c37e0554e3193bc3f0
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-22 23:19:26 +0200

    now it prints characters and strings

---
+++ print.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_string - prints a string without printf
+ *
+ * @s: the string
+ * Return: the number of characters printed
+ */
+int print_string(char *s)
+{
+    int i;
+
+    for (i = 0; s[i]; i++)
+        _putchar(s[i]);
+    return (i);
+}
+
+/**
+ * _printf - prints
+ *
+ * @format: the formatter string
+ * Return: the nubmer of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    unsigned long int i, printed = 0;
+
+    va_start(args, format);
+    for (i = 0; format[i]; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'c') /* character placeholder */
+                _putchar(va_arg(args, int));
+            else if (format[i + 1] == 's') /* string placeholder */
+                print_string(va_arg(args, char *));
+            else /* just the percentage character */
+                _putchar('%');
+        }
+        else /* usual character */
+            _putchar(format[i]);
+    }
+    va_end(args);
+
+    return printed;
+}
