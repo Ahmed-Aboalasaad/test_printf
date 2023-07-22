c658bde822186899f40aa23b31be43937b882a88
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-07-22 20:38:36 +0200

    initial files

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - prints
+ *
+ * @format: the formatter string
+ * Return: the nubmer of characters printed
+ */
+int _printf(const char *format, ...)
+{
+}
