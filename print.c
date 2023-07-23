ae53a7d80a64a75b83cf1335e7bb9c18f4fc20a8
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-07-23 04:40:05 -0700

    What you think about it

--- print.c
+++ print.c
@@ -1,54 +1,69 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
 
-/**
- * print_string - prints a string without printf
- *
- * @s: the string
- * Return: the number of characters printed
- */
 int print_string(char *s)
 {
     int i;
 
     for (i = 0; s[i]; i++)
-        _putchar(s[i]);
-    return (i);
+        putchar(s[i]);
+    return i;
 }
 
 /**
  * _printf - prints
  *
  * @format: the formatter string
- * Return: the nubmer of characters printed
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
     va_list args;
     unsigned long int i, printed = 0;
+    char buffer[1024]; // Temporary buffer for integer to string conversion
 
     va_start(args, format);
     for (i = 0; format[i]; i++)
     {
         if (format[i] == '%')
         {
-            if (format[i + 1] == 'c') /* character placeholder */
-                printed += _putchar(va_arg(args, int));
-            else if (format[i + 1] == 's') /* string placeholder */
-	      
-	      /////////////////////////////////////////////////////
-	      
-	   else if (format[i + 1] == 'd' || *format == 'i') /* integer placeholder */
-                printed += _putchar(va_arg(args, int));
-	   
-	   		
-            else if (format[i + 1] == 'i') /* integer placeholder */
+            i++; // Move past the '%'
+            if (format[i] == '\0')
+                break;
+
+            if (format[i] == 'c')
+            {
+                printed += putchar(va_arg(args, int));
+            }
+            else if (format[i] == 's')
+            {
                 printed += print_string(va_arg(args, char *));
+            }
+            else if (format[i] == 'd' || format[i] == 'i')
+            {
+                sprintf(buffer, "%d", va_arg(args, int)); // Convert integer to string
+                printed += print_string(buffer); // Print the string
+            }
+            else
+            {
+                putchar('%');
+                putchar(format[i]);
+                printed += 2;
+            }
         }
         else /* usual character */
-            _putchar(format[i]);
+        {
+            putchar(format[i]);
+            printed++;
+        }
     }
     va_end(args);
 
     return printed;
+}
+
+int main()
+{
+    _printf("Hello, %s! The answer is %d.\n", "John", 42);
+    return 0;
 }
